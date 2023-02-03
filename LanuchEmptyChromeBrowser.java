28c59a7fe2c0cef6ae6fd858903e924b989aece4
BhavyaUM <123004773+BhavyaUM@users.noreply.github.com>
2023-02-03 08:57:33 +0530

    Add files via upload

---
+++ LanuchEmptyChromeBrowser.java
@@ -0,0 +1,12 @@
+package basics;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LanuchEmptyChromeBrowser {
+public static void main(String[] args) {
+	//instantiate the browser specific class
+	//object creation statement
+	WebDriver driver=new ChromeDriver();
+}
+}
